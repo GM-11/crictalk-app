daacc1e2bc4da21ae496bfbd2d20c34414158a03
GM-11 <gmathur1001@gmail.com>
2023-05-21 11:26:58 +0530

    added topic manager class

--- lib/db/topicmanager.dart
+++ lib/db/topicmanager.dart
@@ -5,16 +5,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class TopicManager {
   static Future<void> createNewTopic(String topic) async {
     try {
-      // Create a new document for the topic
       DocumentReference topicRef =
           FirebaseFirestore.instance.collection('topics').doc();
 
-      // Set the topic data in Firestore
-      await topicRef.set({
-        'title': topic,
-      });
+      await topicRef.set({'topic': topic, 'createdAt': DateTime.now()});
     } catch (error) {
-      // Handle error
       log('Error creating topic: $error');
     }
   }
